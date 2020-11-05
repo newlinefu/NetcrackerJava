9c7a5e36e857e99cb8787609da7b5a40aa25bf5b
Alexandr Smirnov <newlinefu@gmail.com>
2020-11-05 15:38:01 +0300

    Done v_1.0

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,9 @@
-import utils.BubbleSorter;
-
-import java.util.Comparator;
-
 public class Main {
     public static void main(String[] args) {
         /*
             Check second branch
          */
+
     }
+
 }
